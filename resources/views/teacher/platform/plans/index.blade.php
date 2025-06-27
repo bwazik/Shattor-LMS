efa8b6e56ab0567f39910310bcb3d9d407398d18
bwazik <bwazik@outlook.com>
2025-06-27 20:55:50 +0300

    Plans edits

--- resources/views/teacher/platform/plans/index.blade.php
+++ resources/views/teacher/platform/plans/index.blade.php
@@ -64,7 +64,7 @@
                                     @endif
                                     <div class="{{ $plan->id === 3 ? 'my-5 pt-6' : 'mt-3 mb-5' }} text-center">
                                         @if ($plan->id === 1)
-                                            <img src="{{ asset('assets/img/illustrations/pricing-basic.png') }}"
+                                            <img src="{{ asset('assets/img/illustrations/pricing-standard.png') }}"
                                                 alt="{{ $plan->name }}" height="100" />
                                         @elseif($plan->id === 2)
                                             <img src="{{ asset('assets/img/illustrations/pricing-standard.png') }}"
