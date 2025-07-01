64453c3b9baea24804c8204a668144aff98b4db3
bwazik <bwazik@outlook.com>
2025-07-01 04:32:49 +0300

    fix

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('subscriptions:cancel-expired')->dailyAt('02:00');
 Schedule::command('app:update-overdue-invoices')->dailyAt('02:00');
-Schedule::command('lessons:update-status')->everyThirtyMinutes();
+Schedule::command('lessons:update-status')->everyMinute();
