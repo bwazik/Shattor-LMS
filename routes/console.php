a6396fac1bd5e404c00c2a5957714060f537ee53
bwazik <bwazik@outlook.com>
2025-07-01 04:09:06 +0300

    Schedule commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command('subscriptions:cancel-expired')->dailyAt('02:00');
+Schedule::command('lessons:generate')->dailyAt('02:00');
+Schedule::command('lessons:update-status')->dailyAt('02:00');
+Schedule::command('app:update-overdue-invoices')->dailyAt('02:00');
