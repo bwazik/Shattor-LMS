8d5c31a3bbc34ffd45055fbf2b75f7e50012fb7e
bwazik <bwazik@outlook.com>
2025-09-09 14:00:46 +0300

    numeric password

--- public/assets/vendor/js/custom.js
+++ public/assets/vendor/js/custom.js
@@ -227,27 +227,12 @@ function generateRandomUsername(suffix) {
 }
 
 function generateStrongPassword(length = 12) {
-    // Define character sets
-    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const numbers = '0123456789';
-
-    // Combine all characters
-    const allChars = lowercase + uppercase + numbers;
-
-    // Ensure at least one character from each set
     let password = '';
-    password += lowercase.charAt(Math.floor(Math.random() * lowercase.length));
-    password += uppercase.charAt(Math.floor(Math.random() * uppercase.length));
-    password += numbers.charAt(Math.floor(Math.random() * numbers.length));
 
-    // Fill the rest with random characters
-    for (let i = 4; i < length; i++) {
-        password += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    for (let i = 0; i < length; i++) {
+        password += numbers.charAt(Math.floor(Math.random() * numbers.length));
     }
-
-    // Shuffle the password
-    password = password.split('').sort(() => 0.5 - Math.random()).join('');
 
     return password;
 }
