47aed60ccfc0a741c313ee34a2730a29017022a8
bwazik <bwazik@outlook.com>
2025-10-04 01:06:28 +0300

    test3

--- app/Console/Commands/SendBirthdayMessages.php
+++ app/Console/Commands/SendBirthdayMessages.php
@@ -13,21 +13,11 @@ class SendBirthdayMessages extends Command
 
     public function handle()
     {
-        $today = Carbon::today();
-        $eligibleStudents = Student::whereYear('birth_date', '<', $today->year)->get();
+        $studentsWithValidBirthday = Student::query()
+            ->whereNotNull('birth_date')
+            ->whereYear('birth_date', '<', now()->year - 1) // exclude 2025, 2026
+            ->get();
 
-        $nearest = $eligibleStudents->map(function ($student) use ($today) {
-            $nextBirthday = Carbon::createFromDate($today->year, $student->birth_date->month, $student->birth_date->day);
-            if ($nextBirthday->isPast()) {
-                $nextBirthday->addYear();
-            }
-            return [
-                'student' => $student,
-                'next_birthday' => $nextBirthday,
-                'days_until' => $today->diffInDays($nextBirthday),
-            ];
-        })->sortBy('days_until')->take(10);
-
-        dd($nearest);
+        dd($studentsWithValidBirthday);
     }
 }
