2c977e16b0fc0f2a5f0640b1e442eeb2678ef5a8
Abdullah <bwazik@outlook.com>
2025-10-28 04:26:41 +0300

    Update username input context in registration form for consistency

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,7 +11,7 @@
     <p class="mb-5">{{ trans('layouts/login.sign_up_prompt') }}</p>
     <form id="registerForm" class="mb-5" method="POST" action="{{ route('register') }}">
         @csrf
-        <x-basic-input context="modal" type="text" name="username" label="{{ trans('main.username') }}" placeholder="{{ trans('admin/students.placeholders.username') }}" required/>
+        <x-basic-input context="offcanvas" type="text" name="username" label="{{ trans('main.username') }}" placeholder="{{ trans('admin/students.placeholders.username') }}" required/>
         <x-basic-input context="offcanvas" type="password" name="password" label="{{ trans('main.password') }}" required/>
         <div class="mb-5">
             <button class="btn btn-primary d-grid w-100" type="submit">{{ trans('layouts/login.sign_in') }}</button>
