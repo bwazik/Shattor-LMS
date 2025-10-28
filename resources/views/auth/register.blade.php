0b0e68110418c61ba1d1687fee44c9353442e28d
Abdullah <bwazik@outlook.com>
2025-10-28 04:26:21 +0300

    Refactor registration view to use custom input components for improved consistency and readability

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,15 +11,7 @@
     <p class="mb-5">{{ trans('layouts/login.sign_up_prompt') }}</p>
     <form id="registerForm" class="mb-5" method="POST" action="{{ route('register') }}">
         @csrf
-        <div class="form-floating form-floating-outline mb-5">
-            <input type="text" class="form-control @error('username') is-invalid @enderror" id="username"
-                name="username" placeholder="{{ trans('layouts/login.placeholders.username') }}" autofocus
-                required />
-            <label for="username">{{ trans('layouts/login.username') }}</label>
-            @error('username')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
+        <x-basic-input context="modal" type="text" name="username" label="{{ trans('main.username') }}" placeholder="{{ trans('admin/students.placeholders.username') }}" required/>
         <x-basic-input context="offcanvas" type="password" name="password" label="{{ trans('main.password') }}" required/>
         <div class="mb-5">
             <button class="btn btn-primary d-grid w-100" type="submit">{{ trans('layouts/login.sign_in') }}</button>
